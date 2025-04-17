c210b70029477041c4fd60fda72580e17a490b98
Chatzo <chatzoglakis@gmail.com>
2025-04-17 17:05:00 +0200

    Added Quick Sort
    Added an in-place quick sort with test cases.

---
+++ Sorting_algo/QuickSort.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_algo
+{
+    /// <summary>
+    /// In-place quick sort
+    /// </summary>
+    public class QuickSort : ISort<int>
+    {
+        
+        public int[] Sort(int[] unsorted)
+        {
+            if (unsorted == null || unsorted.Length <= 1)
+                return unsorted;
+
+            QuickSortRecursive(unsorted, 0, unsorted.Length - 1);
+            return unsorted;
+        }
+
+        private int[] QuickSortRecursive(int [] unsorted, int startHead, int startTail)
+        {
+            if (startHead >= startTail)
+                return unsorted;
+
+            int first = unsorted[startHead];
+            int middle = unsorted[(startHead + startTail) / 2];
+            int last = unsorted[startTail];
+            //picking the pivot with median of three
+            int pivot;
+            if (first > middle)
+            {
+                if (middle > last)
+                    pivot = middle;  // first > middle > last 
+                else if (first > last)
+                    pivot = last;    // first > last > middle
+                else
+                    pivot = first;   // last > first > middle
+            }
+            else
+            {
+                if (first > last)
+                    pivot = first;   // middle > first > last
+                else if (middle > last)
+                    pivot = last;    // middle > last > first
+                else
+                    pivot = middle;  // last > middle > first
+            }
+
+            int temp;
+            int head = startHead;
+            int mid = startHead;
+            int tail = startTail;
+            
+            while (mid <= tail)
+            {
+                if (unsorted[mid] < pivot)
+                {
+                    temp = unsorted[head];
+                    unsorted[head] = unsorted[mid];
+                    unsorted[mid] = temp;
+                    head++;
+                    mid++;
+                }
+                else if (unsorted[mid] == pivot)
+                {
+                    mid++;
+                }
+                else if (unsorted[mid] > pivot)
+                {
+                    temp = unsorted[tail];
+                    unsorted[tail] = unsorted[mid];
+                    unsorted[mid] = temp;
+                    tail--;
+                } 
+            }
+
+            QuickSortRecursive(unsorted, startHead, head -1 ); //left side of pivot new sub array sent in
+            QuickSortRecursive(unsorted, tail + 1, startTail); //right side of pivot. new subarray is sent in
+            return unsorted;
+        }
+    }
+}
---
+++ Sorting_algo_Tests/QuickSortTest.cs
@@ -0,0 +1,96 @@
+﻿using Sorting_algo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_algo_Tests
+{
+    public class QuickSortTest : BaseSortTest
+    {
+        protected override ISort<int> Sorter => new QuickSort();
+        [Fact]
+        protected void QuickSort_EmptyArray_Test()
+        {
+            base.EmptyArray_Test();
+        }
+        [Fact]
+        protected void QuickSort_SingleElementArray_Test()
+        {
+            base.SingleElementArray_Test();
+        }
+        [Fact]
+        protected void QuickSort_IdenticalElements_Test()
+        {
+            base.IdenticalElements_Test();
+        }
+        [Fact]
+        protected void QuickSort_NegativeNumbers_Test()
+        {
+            base.NegativeNumbers_Test();
+        }
+        [Fact]
+        protected void QuickSort_NumbersWithZero_Test()
+        {
+            base.NumbersWithZero_Test();
+        }
+        [Theory]
+        [InlineData(100, 1, 10)]
+        [InlineData(1000, 1, 1000)]
+        [InlineData(10000, 1, 1000)]
+        public void QuickSort_RandomArray_Test(int arraySize, int lowerBound, int upperBound)
+        {
+            base.RandomArray_Test(arraySize, lowerBound, upperBound);
+        }
+
+        [Theory]
+        [InlineData(100, 1, 10)]
+        [InlineData(1000, 1, 1000)]
+        [InlineData(10000, 1, 1000)]
+        public void QuickSort_SequentialArrayWithWrapping_Test(int arraySize, int lowerBound, int upperBound)
+        {
+            base.SequentialArrayWithWrapping_Test(arraySize, lowerBound, upperBound);
+        }
+        [Theory]
+        [InlineData(1000, 10, 1, 100)]
+        [InlineData(1000, 100, 1, 100)]
+        [InlineData(1000, 100, 1, 1000)]
+        public void QuickSort_MultipleRandomArrays_Test(int numberOfTests, int arraySize, int lowerBound, int upperBound)
+        {
+            for (int i = 0; i < numberOfTests; i++)
+            {
+                base.RandomArray_Test(arraySize, lowerBound, upperBound);
+            }
+        }
+        [Theory]
+        [InlineData(1000, 100, 0, 100)]
+        [InlineData(1000, 100, 1, 100)]
+        public void QuickSort_MultipleSequentialArraysWithWrapping_Test(int numberOfTests, int arraySize, int lowerBound, int upperBound)
+        {
+            for (int i = 0; i < numberOfTests; i++)
+            {
+                base.SequentialArrayWithWrapping_Test(arraySize, lowerBound, upperBound);
+            }
+        }
+        [Fact]
+        protected void QuickSort_SortedArray_Test()
+        {
+            base.SortedArray_Test();
+        }
+        [Fact]
+        protected void QuickSort_ReverseSortedArray_Test()
+        {
+            base.ReverseSortedArray_Test();
+        }
+        [Theory]
+        [InlineData(100)]
+        [InlineData(1000)]
+        [InlineData(10000)]
+        [InlineData(100000)]
+        protected void QuickSort_MinMaxArray_Test(int arraySize)
+        {
+            RandomArray_Test(arraySize, int.MinValue, int.MaxValue);
+        }
+    }
+}

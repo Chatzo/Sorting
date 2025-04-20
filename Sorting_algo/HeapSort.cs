7e66c065ed85f80f3a964efcaa1384e2cfc513bd
Chatzo <chatzoglakis@gmail.com>
2025-04-20 14:55:14 +0200

    Added BinaryHeap and HeapSort
    Added BinaryHeap from my other project and Added HeapSort and tested it. all passed

--- Sorting_algo/HeapSort.cs
+++ Sorting_algo/HeapSort.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataStructures;
 
 namespace Sorting
 {
     public class HeapSort: ISort<int>
     {
+
+        BinaryHeap<int> heap = new BinaryHeap<int>();
         public int[] Sort(int[] unsorted)
         {
             //Build the Max-heap
+            heap.BuildHeap(unsorted);
 
             //Sort the Array
+            for (int i = unsorted.Length - 1; i >= 0; i--)
+            {
+                unsorted[i] = heap.Extract();
+            }
+
             return unsorted;
         }
     }
--- Sorting_algo/Program.cs
+++ Sorting_algo/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("If you see this, Run all tests instead of running the program");
         }
     }
 }
